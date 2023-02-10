74b4d833e23ef4e0fc47f0360446d5007d8109da
Scott Bagley <sbagley83@gmail.com>
2023-02-10 10:44:45 -0600

    test function

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+// let rock = 'rock'
+// let scissors = 'scissors'
+// let paper = 'paper'
+
+
+function getComputerChoice (rock,paper,scissors){
+    return ('rock' || 'paper' || 'scissors')
+}
+
+console.log(getComputerChoice)
